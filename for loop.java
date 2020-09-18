26eea4d0e3a7398fa8fbbd676c57e50f624969fd
Jake Cohen <46077226+JakeCohen0427@users.noreply.github.com>
2020-09-18 11:02:19 -0400

    Create for loop.java

---
+++ for loop.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        for(int i = 10; i > 0; i++) {
+            if(i + 1 <= 0) {
+                System.out.println(i);
+                System.out.println(i + 1);
+            }
+        }
+    }
+}
