8862c9dde164cb23dde06504dc252cd8f778e286
Jake Cohen <46077226+JakeCohen0427@users.noreply.github.com>
2020-09-21 09:32:33 -0400

    Create Fibonacci sequence.java

---
+++ Fibonacci sequence.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Please enter the number of fibonacci numbers you would like: ");
+    int p = sc.nextInt();
+    p = p - 1;
+    int x;
+    int y = 0;
+    int z = 1;
+    for(int i = 0; i <= p; i++) {
+      System.out.println(z);
+      x = y + z; 
+      y = z; 
+      z = x; 
+    }
+  }
+}
+/*
+Prompt the user for a number.  
+Calculate and print out that many 
+numbers in fibonacci sequence.
+*/
